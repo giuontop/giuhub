8c290155cf88b8f6aa8d696b4ac83750c1b45ecf
giuontop <gaerogiu77@gmail.com>
2024-05-29 00:23:47 +0200

    Update giuhub.lua

--- giuhub.lua
+++ giuhub.lua
@@ -2,9 +2,9 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
  
  
 local MainWindow = Rayfield:CreateWindow({
-    Name = "Giu Hub",
+    Name = "⚡ Giu Hub ⚡",
     LoadingTitle = "Giu Hub is Loading...",
-    LoadingSubtitle = "by Giu",
+    LoadingSubtitle = "by ⚡ Giu ⚡",
     ConfigurationSaving = {
        Enabled = true,
        FolderName = nil, -- Create a custom folder for your hub/game
@@ -17,7 +17,7 @@ local MainWindow = Rayfield:CreateWindow({
     },
     KeySystem = true, -- Set this to true to use our key system
     KeySettings = {
-       Title = "Giu Hub",
+       Title = "⚡ Giu Hub ⚡",
        Subtitle = "Key System",
        Note = "You can find the key here: discord.gg/tFicrmp0",
        FileName = "GiuHubKey",
@@ -32,59 +32,56 @@ local MainWindow = Rayfield:CreateWindow({
  
  
  local Button = MainTab:CreateButton({
-    Name = "Murder VS Sheriff",
+    Name = "⚡ Murder VS Sheriff ⚡",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://pastebin.com/raw/UjcfSzdH"))()
 
     end, 
     })
 local Button = MainTab:CreateButton({
-    Name = "MM2",
+    Name = "⚡ MM2 ⚡",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://raw.githubusercontent.com/mezzopera/OMEN-MM2/main/omenmm2"))()
 
     end, 
     })
  local Button = MainTab:CreateButton({
-    Name = "Arsenal",
+    Name = "⚡ Arsenal ⚡",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://pastebin.com/raw/4DhBNXdm"))()
 
     end, 
     })
  local Button = MainTab:CreateButton({
-    Name = "Dupe Script",
+    Name = "⚡ Dupe Script ⚡",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://pastebin.com/raw/qchSrRsW"))()
 
     end, 
     })
  local Button = MainTab:CreateButton({
-    Name = "iShield",
+    Name = "⚡ iShield ⚡",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://raw.githubusercontent.com/iShield2005/scripts/master/shieldhubloader"))()
 
     end, 
     })
  local Button = MainTab:CreateButton({
-    Name = "Azure Modded",
+    Name = "⚡ Azure Modded ⚡",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))()
 
     end, 
     })
  local Button = MainTab:CreateButton({
-    Name = "Da Hood Polakya",
+    Name = "⚡ Da Hood Polakya ⚡",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelheadx/Polakya/main/Bestscript.md"))();
     end, 
     })
  local Button = MainTab:CreateButton({
-    Name = "Blade Ball",
+    Name = "⚡ Blade Ball ⚡",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://pastebin.com/raw/tw5if3qA"))()
     end
  })
- 
- 
- 
